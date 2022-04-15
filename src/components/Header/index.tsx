ca59a77995372c4c1fc2c1c15d154b4a7f6a7990
mateusx111 <mateuszmt@outlook.com>
2022-04-14 23:10:59 -0300

    w

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
 export function Header() {
+  const [isModalOpen] = useState
+ 
   return (
     <Container>
       <Content>
