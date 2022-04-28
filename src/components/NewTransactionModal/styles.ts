c325f365f4f67d5c39a0968bda5ce91b701cc05f
mateusx111 <mateuszmt@outlook.com>
2022-04-28 10:09:36 -0300

    processing

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-import { darken } from 'polished'
+import { darken,transparentize } from 'polished'
+
 export const Container = styled.form`
   h2{
     color: var(--text-title);
@@ -56,7 +57,13 @@ export const TransactionTypeContainer = styled.div`
 `;
 
 interface RadioBoxProps{
-  isActive: boolean
+  isActive: boolean;
+  activeColor: 'green'|'red';
+}
+
+const colors = {
+  green: '#33CC95',
+  red: '#E52E4D'
 }
 
 export const RadioBox =styled.button<RadioBoxProps>`
@@ -64,7 +71,7 @@ export const RadioBox =styled.button<RadioBoxProps>`
     border: 1px solid #d7d7d7;
     border-radius: 0.25rem;
 
-    background: ${(props) => props.isActive ? '#ccc' : 'transparent'};
+    background: ${(props) => props.isActive ? transparentize(0.9, colors[props.activeColor]) : 'transparent'};
 
     display: flex;
     align-items: center;
